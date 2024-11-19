9b54b370dfa724e835bb788c91e0de6e43b8cac9
JulianKimmig <julian.kimmig@gmx.net>
2024-11-19 15:33:42 +0100

    test1

---
+++ tests/test_ds.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+
+
+class TestDS(TestCase):
+    def test_BradleyDoublePlusGoodMP(self):
+        from moldata.datasets.pysical.mp import BradleyDoublePlusGoodMP
+
+        loader = BradleyDoublePlusGoodMP()
+        i = 0
+        for mol, prop in loader.iterate_with_property("mpC", return_list=True):
+            i += 1
+            self.assertIsInstance(prop, list)
+            for entry in prop:
+                self.assertIsInstance(entry, float)
+
+        self.assertEqual(i, loader.expected_mol)
